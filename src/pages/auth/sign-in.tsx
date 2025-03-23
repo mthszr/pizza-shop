e6dec42f27228d1bf6b15f03fea30439a5f82a55
Matheus Barbosa <mamateubosa00@gmail.com>
2025-03-23 19:52:29 -0300

    add sign-up page and routing, include form validation and success notification

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -3,6 +3,7 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -37,7 +38,10 @@ export function SignIn() {
   return (
     <>
       <Helmet title="Login" />
-      <div className="p-8 ">
+      <div className="p-8">
+        <Button variant="outline" asChild className="absolute top-8 right-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="font-semibold text-2xl tracking-tight">
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter } from 'react-router-dom'
-
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard/dashboard'
 import { SignIn } from './pages/auth/sign-in'
+import { SignUp } from './pages/auth/sign-up'
 
 export const router = createBrowserRouter([
   {
@@ -25,7 +25,10 @@ export const router = createBrowserRouter([
         path: '/sign-in',
         element: <SignIn />,
       },
+      {
+        path: '/sign-up',
+        element: <SignUp />,
+      },
     ],
   },
-
 ])
