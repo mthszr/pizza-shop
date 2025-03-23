7cc6cef4a78d0fa4a47de7ec9116710718ed6d44
Matheus Barbosa <mamateubosa00@gmail.com>
2025-03-23 18:19:38 -0300

    add react-helmet-async for document head management and update app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { Button } from './components/ui/button'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { RouterProvider } from 'react-router-dom'
 import './global.css'
+import { router } from './routes'
 
 export function App() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <Button>Enviar</Button>
-    </div>
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
   )
 }
