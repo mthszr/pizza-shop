f213b941a718c4b5dd70992c453f01f4c407fc7d
Matheus Barbosa <mamateubosa00@gmail.com>
2025-03-24 19:10:26 -0300

    add theme toggle and account menu components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
+import { ThemeProvider } from './components/theme/theme-provider'
 import './global.css'
 import { router } from './routes'
-import { Toaster } from 'sonner'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <Toaster />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <Toaster />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
